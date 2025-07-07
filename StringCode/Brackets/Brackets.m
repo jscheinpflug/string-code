f3041ed2a499c2b2f67eb52bb63b82657f7f0394
Jaroslav Scheinpflug <jscheinpflug@g.harvard.edu>
2025-07-07 12:45:37 +0200

    feature: add b0^- action to 2-bracket

--- StringCode/Brackets/Brackets.m
+++ StringCode/Brackets/Brackets.m
@@ -38,7 +38,7 @@ If[RtestUpToConstant[OPEpart],
 tayloredOPEpart = If[powerHol<0, 
 If[powerAntiHol< 0,TaylorAtOrder[OPEpart,-powerHol, -powerAntiHol,0,0], TaylorAtOrder[OPEpart/.{z0bar->0},-powerHol, 0,0,0]], 
 If[powerAntiHol <0, TaylorAtOrder[OPEpart/.{z0->0},0, -powerAntiHol,0,0], OPEpart/.{z0->0,z0bar->0}]]//Expand;
-result = result + tayloredOPEpart;,
+result = result + b0m[tayloredOPEpart];,
 0];
 ],List @@((OPE[SFAtPos[SFa, w0, w0bar], SFAtPos[SFb,z0,z0bar]])/.{w0->-z0,w0bar->-z0bar}//Expand)]; result];
 
@@ -47,6 +47,36 @@ Bracket[a_+b_,c_]:=Bracket[a,c]+Bracket[b,c]
 Bracket[a_,b_+c_]:=Bracket[a,b]+Bracket[a,c]
 Bracket[a_ b_,c_]:=a Bracket[b,c]/;(And @@(FreeQ[a,#]&/@ allfields))
 Bracket[a_,b_ c_]:=b Bracket[a,c]/;(And @@(FreeQ[b,#]&/@ allfields))
+
+
+(* ::Subsection:: *)
+(*Define action of b0^-*)
+
+
+b0mHolo[Ra_/;Rtest[Ra]] := Module[{result, RList = List @@ Ra, pos, numberOfFermions},
+pos = FirstPosition[RList, c[1, 0], None];
+If[pos === None, Return[0]];
+numberOfFermions = Count[
+    Take[RList, pos[[1]] - 1],
+    f_ /; MemberQ[fermions, Head[f]]
+  ];
+  result = (-1)^numberOfFermions R @@ Delete[RList, pos[[1]]];
+  result];
+
+b0mAntiHolo[Ra_/;Rtest[Ra]] := Module[{result, RList = List @@ Ra, pos, numberOfFermions},
+pos = FirstPosition[RList, ct[1, 0], None];
+If[pos === None, Return[0]];
+numberOfFermions = Count[
+    Take[RList, pos[[1]] - 1],
+    f_ /; MemberQ[fermions, Head[f]]
+  ];
+  result = (-1)^numberOfFermions R @@ Delete[RList, pos[[1]]];
+  result];
+
+b0m[Ra_/;Rtest[Ra]] := b0mHolo[Ra] - b0mAntiHolo[Ra];
+b0m[a_+b_]:=b0m[a] + b0m[b];
+b0m[a_ b_]:=a b0m[b]/;(And @@(FreeQ[a,#]&/@ allfields))
+b0m[0] := 0;
 
 
 (* ::Section:: *)
