a2d38e35fbb88be464ec9d7295939c43d02d94d6
Jaroslav Scheinpflug <jscheinpflug@g.harvard.edu>
2025-10-28 22:53:30 -0400

    fix: power of plumbing parameter in propagator

--- StringCode/Brackets/Brackets.m
+++ StringCode/Brackets/Brackets.m
@@ -126,7 +126,7 @@ a BracketProjection[b, weightHolo, weightAntiHolo] /; And @@ (FreeQ[a, #] & /@ J
 BracketProjection[WedgeProduct[a__] b___, weightHolo_, weightAntiHolo_] := WedgeProduct[a, BracketProjection[b, weightHolo, weightAntiHolo]]
 
 
-(* ::Subsection::Closed:: *)
+(* ::Subsection:: *)
 (*Collapse multi-local operator via OPE*)
 
 
@@ -631,19 +631,23 @@ ApplyPropagator::usage = "Applies the propagator b0+/L0+ on a level-projected br
 ApplyPropagator[q_][a_ + b_]:= ApplyPropagator[q][a] + ApplyPropagator[q][b]
 ApplyPropagator[q_][a_ b_]:= a ApplyPropagator[q][b]/;(And @@(FreeQ[a,#]&/@ allfields));
 
-ApplyPropagator[q_][Ra_/;Rtest[Ra]]:= Module[{rescaledR},
-rescaledR = mapOp[rescaling[q], rescaling[Conjugate[q]]][Ra];
-actBGhostMode[bmodeHolo[0], rescaledR] + actBGhostMode[bmodeAntiHolo[0],rescaledR]]
-
-ApplyPropagator[q_][Opa_/;OpTest[Opa]]:= Module[{rescaledOp},
-rescaledOp = mapOp[rescaling[q], rescaling[Conjugate[q]]][Opa];
-actBGhostMode[bmodeHolo[0], rescaledOp] + actBGhostMode[bmodeAntiHolo[0],rescaledOp]]
-
 ApplyPropagator[q_][MultiOpa_/;MultiOptest[MultiOpa]]:= Module[{rescaledMultiOp},
-rescaledMultiOp = mapOp[rescaling[q], rescaling[Conjugate[q]]][MultiOpa];
+rescaledMultiOp =  1/(q Conjugate[q]) mapOp[rescaling[q], rescaling[Conjugate[q]]][MultiOpa];
 actBGhostMode[bmodeHolo[0], rescaledMultiOp] + actBGhostMode[bmodeAntiHolo[0],rescaledMultiOp]]
 
-rescaling[factor_][z_]:= factor z;
+ApplyPropagator[q_][Opa_/;OpTest[Opa]]:= Module[{rescaledOp},
+rescaledOp = 1/(q Conjugate[q]) mapOp[rescaling[q], rescaling[Conjugate[q]]][Opa];
+actBGhostMode[bmodeHolo[0], rescaledOp] + actBGhostMode[bmodeAntiHolo[0],rescaledOp]]
+
+ApplyPropagator[q_][Ra_/;Rtest[Ra]]:= Module[{rescaledR},
+rescaledR = 1/(q Conjugate[q]) mapOp[rescaling[q], rescaling[Conjugate[q]]][Ra];
+actBGhostMode[bmodeHolo[0], rescaledR] + actBGhostMode[bmodeAntiHolo[0],rescaledR]]
+
+ApplyPropagator[q_][Ia_/;InteractingTest[Ia]]:= Module[{rescaledI},
+rescaledI = 1/(q Conjugate[q]) mapOp[rescaling[q], rescaling[Conjugate[q]]][Ia];
+actBGhostMode[bmodeHolo[0], rescaledI] + actBGhostMode[bmodeAntiHolo[0],rescaledI]]
+
+rescaling[factor_][z_]:= factor z //Expand;
 
 
 (* ::Subsection::Closed:: *)
