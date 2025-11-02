e2fd976e306f29861c198ca40e01831e3220b622
Jaroslav Scheinpflug <jscheinpflug@g.harvard.edu>
2025-11-02 14:28:05 -0500

    fix: add q^L0 qbar^Lobar

--- StringCode/Operators/Operators.m
+++ StringCode/Operators/Operators.m
@@ -112,18 +112,36 @@ parityOp[Ia_/;InteractingTest[Ia]]:= Mod[parity[Ia],2];
 
 
 (* ::Subsection:: *)
-(*Define weight of Interacting operators*)
+(*Define weight of operators*)
 
 
 totalWeightHolo::usage = "Computes total holomorphic weight of a normal-ordered product";
 totalWeightAntiHolo::usage = "Computes total holomorphic weight of a normal-ordered product";
 totalWeight::usage = "Computes total weight of a normal-ordered product";
 
+totalWeightHolo[Times[a_, MultiOpa_/;MultiOptest[MultiOpa]]] := totalWeightHolo[MultiOpa];
+totalWeightHolo[MultiOpa_/;MultiOptest[MultiOpa]] := Total[Map[totalWeightHolo, List @@ MultiOpa]];
+
+totalWeightAntiHolo[Times[a_, MultiOpa_/;MultiOptest[MultiOpa]]] := totalWeightAntiHolo[MultiOpa];
+totalWeightAntiHolo[MultiOpa_/;MultiOptest[MultiOpa]] := Total[Map[totalWeightAntiHolo, List @@ MultiOpa]];
+
+totalWeightHolo[Times[a_, Opa_/;OpTest[Opa]]] := totalWeightHolo[Opa];
+totalWeightHolo[Opa_/;OpTest[Opa]] := totalWeightHolo[Opa[[1]]] + totalWeightHolo[Opa[[2]]];
+
+totalWeightAntiHolo[Times[a_, Opa_/;OpTest[Opa]]] := totalWeightAntiHolo[Opa];
+totalWeightAntiHolo[Opa_/;OpTest[Opa]] := totalWeightAntiHolo[Opa[[1]]] + totalWeightAntiHolo[Opa[[2]]];
+
 totalWeightHolo[Times[a_, Ia_/;InteractingTest[Ia]]] := totalWeightHolo[Ia];
 totalWeightHolo[Ia_/;InteractingTest[Ia]] := Map[weightHolo, List @@ Ia] // Total;
 
 totalWeightAntiHolo[Times[a_, Ia_/;InteractingTest[Ia]]] := totalWeightAntiHolo[Ia];
 totalWeightAntiHolo[Ia_/;InteractingTest[Ia]] := Map[weightAntiHolo, List @@ Ia] // Total;
+
+totalWeight[Times[a_, MultiOpa_/;MultiOptest[MultiOpa]]] := totalWeight[MultiOpa];
+totalWeight[MultiOpa_/;MultiOptest[MultiOpa]] := {totalWeightHolo[MultiOpa], totalWeightAntiHolo[MultiOpa]};
+
+totalWeight[Times[a_, Opa_/;OpTest[Opa]]] := totalWeight[Opa];
+totalWeight[Opa_/;OpTest[Opa]] := {totalWeightHolo[Opa], totalWeightAntiHolo[Opa]};
 
 totalWeight[Times[a_, Ia_/;InteractingTest[Ia]]] := totalWeight[Ia];
 totalWeight[Ia_/;InteractingTest[Ia]] := {totalWeightHolo[Ia], totalWeightAntiHolo[Ia]};
