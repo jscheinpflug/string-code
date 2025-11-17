b1e4bb9be81217491896f048f842f43d80cd1ded
Jaroslav Scheinpflug <jscheinpflug@g.harvard.edu>
2025-11-17 14:23:49 -0500

    refactor: uniform definition of stub

--- StringCode/Brackets/Bosonic/Flat/Flat.m
+++ StringCode/Brackets/Bosonic/Flat/Flat.m
@@ -26,6 +26,9 @@ Needs["StringCode`Brackets`"];
 (*Logic*)
 
 
+r0::usage = "A stub parameter";
+
+
 Begin["Private`"];
 
 
@@ -34,10 +37,10 @@ Begin["Private`"];
 
 
 flatLocalCoordinate::usage = "Creates i-th holomorphic flat local coordinate";
-flatLocalCoordinate[order_, i_][moduli___][w_] := w Symbol["Private`q"][order, i][moduli] + Symbol["Private`z"][order, i][moduli];
+flatLocalCoordinate[order_, i_][moduli___][w_] := w r0 Symbol["Private`q"][order, i][moduli] + Symbol["Private`z"][order, i][moduli];
 
 flatLocalCoordinateBar::usage = "Creates i-th antiholomorphic flat local coordinate";
-flatLocalCoordinateBar[order_, i_][moduli___][wbar_] := wbar Symbol["Private`qbar"][order, i][moduli] + Symbol["Private`zbar"][order, i][moduli];
+flatLocalCoordinateBar[order_, i_][moduli___][wbar_] := wbar r0 Symbol["Private`qbar"][order, i][moduli] + Symbol["Private`zbar"][order, i][moduli];
 
 
 localCoordinateReplacementElem::usage = "Replaces abstract local coordinates with their actual moduli dependence";
@@ -80,10 +83,10 @@ localCoordinateReplacement = Join[localCoordinateReplacement, localCoordinateRep
 
 
 Module[{z0, z0bar, r0},
-qR[2,1][]:= r0;
-qR[2,2][]:= r0;
-qbarR[2,1][]:= r0;
-qbarR[2,2][]:= r0;
+qR[2,1][]:= 1;
+qR[2,2][]:= 1;
+qbarR[2,1][]:= 1;
+qbarR[2,2][]:= 1;
 zR[2,1][]:= - z0;
 zR[2,2][]:= z0;
 zbarR[2,1][]:= - z0bar;
