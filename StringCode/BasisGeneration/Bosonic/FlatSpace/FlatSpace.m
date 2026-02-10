19a498d20d67ad6d589239275a77fca7652a348b
Jaroslav Scheinpflug <jscheinpflug@g.harvard.edu>
2026-02-10 14:37:05 -0500

    chore: create files for BasisGeneration

---
+++ StringCode/BasisGeneration/Bosonic/FlatSpace/FlatSpace.m
@@ -0,0 +1,35 @@
+(* ::Package:: *)
+
+(* ::Section:: *)
+(*Init*)
+
+
+BeginPackage["StringCode`BasisGeneration`Bosonic`FlatSpace`"];
+
+
+Needs["StringCode`Symbols`"];
+Needs["StringCode`Symbols`Bosonic`"];
+Needs["StringCode`Symbols`Bosonic`FlatSpace`"];
+Needs["StringCode`NormalOrdering`"];
+Needs["StringCode`NormalOrdering`Bosonic`"];
+
+(* ::Section:: *)
+(*Declare public variables and methods*)
+
+
+(* ::Section:: *)
+(*Logic*)
+
+
+(* ::Input::Initialization:: *)
+Begin["Private`"];
+
+
+(* ::Section:: *)
+(*End*)
+
+
+End[];
+
+
+EndPackage[];
---
+++ StringCode/BasisGeneration/TypeII/FlatSpace/FlatSpace.m
@@ -0,0 +1,35 @@
+(* ::Package:: *)
+
+(* ::Section:: *)
+(*Init*)
+
+
+BeginPackage["StringCode`BasisGeneration`TypeII`FlatSpace`"];
+
+
+Needs["StringCode`Symbols`"];
+Needs["StringCode`Symbols`TypeII`"];
+Needs["StringCode`Symbols`TypeII`FlatSpace`"];
+Needs["StringCode`NormalOrdering`"];
+Needs["StringCode`NormalOrdering`TypeII`"];
+
+(* ::Section:: *)
+(*Declare public variables and methods*)
+
+
+(* ::Section:: *)
+(*Logic*)
+
+
+(* ::Input::Initialization:: *)
+Begin["Private`"];
+
+
+(* ::Section:: *)
+(*End*)
+
+
+End[];
+
+
+EndPackage[];
